0e3d167d25ff89d5f49e2134af9eb55e98583b9c
CEAmarketplace <tergel.eb@gmail.com>
2026-04-02 19:54:31 +0800

    fix favicon filename

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,6 @@ export const metadata: Metadata = {
   title: "CEA United",
   description:
     "CEA United develops flexible, practical, and scalable business solutions.",
-  icons: {
-    icon: "/cea_favicon.ico",
-    shortcut: "/cea_favicon.ico",
-    apple: "/cea_favicon.ico",
-  },
 };
 
 export default function RootLayout({
